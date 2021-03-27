488f449c2c370b41f3475f014f1c6ed7b2380240
Mateusz Napieralski <mateusz.napieralski04@gmail.com>
2021-03-27 19:49:53 +0100

    Create footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import moment from 'moment';
+import React from 'react';
+import styles from '../styles/components/Footer.module.scss';
+import NextLogo from '../assets/next-js.svg';
 
 const Footer = () => {
+  const year = moment().year();
+
   return (
-    <footer>
-      <div>test</div>
+    <footer className={styles.container}>
+      <h3>{`Copyright \u00A9 ${year}`}</h3>
+      <div className={styles.iconsRow}>
+        <h2>Created with</h2>
+        <div className={styles.iconsRow}>
+          <div className={styles.icon}>
+            <img src={NextLogo} />
+          </div>
+          <div className={styles.icon}></div>
+        </div>
+        <h2>by Mateusz Napieralski</h2>
+      </div>
     </footer>
   );
 };
