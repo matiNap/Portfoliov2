d1e400d85a660d5e9015097d26ecfb18d2a3b4ed
Mateusz Napieralski <mateusz.napieralski04@gmail.com>
2021-03-31 19:37:33 +0200

    Add home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,49 @@
 import DefaultHead from '../components/DefaultHead';
 import Background from '../components/Background';
 import withMenu from '../hocs/withMenu';
+import styles from '../styles/Home.module.scss';
+import { useTranslation } from 'react-i18next';
+import TsLogo from '../assets/ts.svg';
+import JsLogo from '../assets/js.svg';
+import { SiReact } from 'react-icons/si';
 
 const Home = () => {
+  const { t } = useTranslation();
   return (
     <div>
       <DefaultHead />
 
       <Background>
-        <div
-          style={{ height: '300vh', width: '30px', backgroundColor: 'blue' }}
-        />
+        <div className={styles.container}>
+          <h1>{t('Hi,')}</h1>
+          <h1>{t('I am Mateusz Napieralski')}</h1>
+          <h1 className={styles.specification}>
+            Front end developer / Mobile developer
+          </h1>
+          <h1 className={styles.toolsHeader}>
+            Frameworks and languages I work with:
+          </h1>
+          <div>
+            <ul>
+              <li>
+                <SiReact color="#60DAFB" size={26} />
+                <p>React</p>
+              </li>
+              <li>
+                <SiReact color="#60DAFB" size={26} />
+                <p>React Native</p>
+              </li>
+              <li>
+                <JsLogo />
+                <p>Javascript</p>
+              </li>
+              <li>
+                <TsLogo />
+                <p>Typescript</p>
+              </li>
+            </ul>
+          </div>
+        </div>
       </Background>
     </div>
   );
